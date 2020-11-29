c510fd1b5431a0b5da8e22bd3909abb624a3e4bf
Shruti Tiwari <sh03shruti@gmail.com>
2020-11-30 01:23:16 +0530

    Added website details

--- Main.js
+++ Main.js
@@ -4,3 +4,10 @@ var preloader = document.getElementById('preloading');
 function codeLoaded() {
     preloader.style.display = 'none';
 }
+
+$('.count').counterUp(
+    {
+        delay: 10,
+        time: 3000,
+    }
+) 
