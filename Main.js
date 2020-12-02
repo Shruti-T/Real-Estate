f5064c351ed0ee0282f4fc35581ecefc4c5f6fd9
Shruti Tiwari <sh03shruti@gmail.com>
2020-12-02 12:08:44 +0530

    Added JS to contact form

--- Main.js
+++ Main.js
@@ -10,4 +10,97 @@ $('.count').counterUp(
         delay: 10,
         time: 3000,
     }
-) 
+)
+
+var cost1 = 0, cost2 = 0, cost3 = 0, cost4 = 0;
+
+
+
+document.getElementById('park').addEventListener('click', function () {
+
+    var check1 = document.getElementById('park');
+
+    if (check1.checked == true) {
+        cost1 = 400;
+    }
+    else {
+        cost1 = 0;
+    }
+    total();
+});
+
+document.getElementById('gym').addEventListener('click', function () {
+    var check2 = document.getElementById('gym');
+
+
+    //console.log(check.checked);
+    if (check2.checked == true) {
+        cost2 = 300;
+    }
+    else {
+        cost2 = 0;
+    }
+    total();
+});
+
+document.getElementById('lib').addEventListener('click', function () {
+    var check3 = document.getElementById('lib');
+
+
+    //console.log(check.checked);
+    if (check3.checked == true) {
+        cost3 = 900;
+    }
+    else {
+        cost3 = 0;
+    }
+    total();
+});
+
+document.getElementById('golf').addEventListener('click', function () {
+
+
+    var check4 = document.getElementById('golf');
+    if (check4.checked == true) {
+        cost4 = 700;
+    }
+    else {
+        cost4 = 0;
+    }
+    total();
+});
+
+function total() {
+    var totalCost = 0;
+    totalCost = cost1 + cost2 + cost3 + cost4;
+    document.getElementById('approx').innerHTML = `Approx. Charges on monthly basis for desired amenities $ ${totalCost}.`;
+}
+
+document.getElementById('submitVisit').onsubmit = function () {
+    alert('Your form has been submitted successfully. Further deatails have been sent on your email.');
+}
+
+
+
+
+
+
+// document.getElementById('bookVisit').onclick = function () {
+
+
+//     var input1 = document.getElementById('inputName');
+//     var input2 = document.getElementById('inputMail');
+//     var input3 = document.getElementById('inputCity');
+//     var input4 = document.getElementById('inputPlot');
+//     var input5 = document.getElementById('inputDate');
+
+//     console.log(input1);
+
+//     if (input1 == true && input2 == true && input3 == true && input4 == true && input5 == true) {
+//         alert('cd');
+//     }
+//     else {
+//         alert('Please fill in the details.');
+//     }
+
+// }
